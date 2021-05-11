7c06a6d1b35ece127ff163a9aff17f98f97a5608
XeretaDe <XeretaDe@users.noreply.github.com>
2021-05-11 00:05:28 -0300

    Mudanças no cálculo, organização e comentários
    Mudanças dentro do While para que o código faça os cálculos corretamente, organização (retirar comentários inúteis, ajustes na formatação) e adição de comentários para explicação do código.

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -17,19 +17,21 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-
+            // Indexes
             int MaxCompartilha = 0;
+            int MaxCPM = 0;
 
+            //Valores Não Atribuidos
             float Investimento;
             float VisusIndiretas;
             float VisusDiretas;
             float ClicksInicial;
             float ClicksIndiretos;
-            float ClicksFinal;
             float Compartilhamentos;
             float CompartilhamentosNew;
-            float CompartilhamentosFinal;
 
+            
+            // Valores Atribuidos
             float PorcentoClicks = 0.12F;
             float PorcentoCompartilhar = 0.15F;
 
@@ -44,67 +46,58 @@ namespace Calculadora
                 Console.WriteLine("Tente inserir um número!");
             }
 
-            Console.WriteLine("O valor de seu investimento é " + Investimento + "R$");
+            Console.WriteLine("O valor de seu investimento é " + Investimento + "R$"); // Base do código
 
             VisusDiretas = Investimento * 30;   // Multiplica o investimento para visualizações Diretas
             ClicksInicial = VisusDiretas * PorcentoClicks;   // Define o numero de clicks por visualização
             Compartilhamentos = ClicksInicial * PorcentoCompartilhar;  // Define o número de compartilhamentos do anúncio
             VisusIndiretas = Compartilhamentos * 40; // Visualizações de compartilhamento
 
-            int[] CalculoDeVisus = new int[4];
-            int[] CalculoDeCompartilhamentos = new int[4];
+
+            int[] CalculoDeVisus = new int[4];   // Criação de arrays
+            int[] CalculoDeCompartilhamentos = new int[3];
             int[] CalculoDeClicks = new int[4];
 
 
+                while (MaxCompartilha < 4){
 
-            if (Compartilhamentos != 0)
-            {
+                ClicksIndiretos = VisusIndiretas * PorcentoClicks;
+                CompartilhamentosNew = ClicksIndiretos * PorcentoCompartilhar;  
+                
+                CalculoDeVisus[MaxCompartilha] = (int)VisusIndiretas; // Definir Array
 
-                while (MaxCompartilha < 4)
-                {
+                VisusIndiretas = CompartilhamentosNew * 40;  
 
-                    ClicksIndiretos = VisusIndiretas * PorcentoClicks;
-                    CompartilhamentosNew = ClicksIndiretos * PorcentoCompartilhar;
-                    CompartilhamentosFinal = CompartilhamentosNew + Compartilhamentos;
-                    ClicksFinal = ClicksIndiretos + ClicksInicial;
 
-                    if (MaxCompartilha != 1)
-                    {
-                        VisusIndiretas = CompartilhamentosNew * 40;
+                    if (MaxCompartilha < 3){
+                    CalculoDeCompartilhamentos[MaxCPM] = (int)CompartilhamentosNew; //Definir array
                     }
 
-                    CalculoDeVisus[MaxCompartilha] = (int)VisusIndiretas;
+                CalculoDeClicks[MaxCompartilha] = (int)ClicksIndiretos; // Definir Array
 
-                    CalculoDeCompartilhamentos[MaxCompartilha] = (int)CompartilhamentosNew;
+                MaxCompartilha++;  // Aumento do index
 
-                    CalculoDeClicks[MaxCompartilha] = (int)ClicksIndiretos;
-
-
-                    //  Console.WriteLine(ClicksIndiretos);
-                    //   Console.WriteLine(CompartilhamentosNew);
-
-                    MaxCompartilha++;
-
-
-                    if (MaxCompartilha == 4)
-                    {
-                    int VisusIndiretas = CalculoDeVisus.Aggregate((a, b) => a + b);
-                    int totalCompartilhar = CalculoDeCompartilhamentos.Aggregate((a, b) => a + b);
-                    int ClicksIndiretos = CalculoDeClicks.Aggregate((a, b) => a + b);
-
-                        Console.WriteLine(VisusIndiretas);
-                        Console.WriteLine(totalCompartilhar);
-                        Console.WriteLine(ClicksIndiretos);
-
-                        // Console.WriteLine("Suas visualizações diretas serão por volta de " + VisusDiretas + " e seu número de Clicks serão aproximadamente " + ClicksFinal);
-                        // Console.WriteLine("Seus Clicks devido a compartilhamentos são: " + ClicksIndiretos + " e os compartilhamentos totais são: " + CompartilhamentosFinal);
+                    if(MaxCompartilha < 3){
+                    MaxCPM++;  // Aumento do index especifico para compartilhamentos
                     }
-
 
                 }
-            }
+   
 
+                float VisusIndiretasTT = CalculoDeVisus.Aggregate((a, b) => a + b);     // Soma dos arrays
+                int totalCompartilhar = CalculoDeCompartilhamentos.Aggregate((a, b) => a + b);
+                int ClicksIndiretosTT = CalculoDeClicks.Aggregate((a, b) => a + b);
+
+                Console.WriteLine(VisusIndiretasTT + VisusDiretas +" Visus");
+                Console.WriteLine(totalCompartilhar + (int)Compartilhamentos + " Compartilhamentos");
+                Console.WriteLine(ClicksIndiretosTT + ClicksInicial + " Clicks");
+
+                // Console.WriteLine("Suas visualizações diretas serão por volta de " + VisusDiretas + " e seu número de Clicks serão aproximadamente " + ClicksFinal);
+                // Console.WriteLine("Seus Clicks devido a compartilhamentos são: " + ClicksIndiretos + " e os compartilhamentos totais são: " + CompartilhamentosFinal);
             
+
+
+
 
 
         }
